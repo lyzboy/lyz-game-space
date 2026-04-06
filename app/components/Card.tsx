bb76327f7f305610beac94cf7ee5acd6ae307806
Josh Sanford <13478602+lyzboy@users.noreply.github.com>
2026-04-05 21:43:47 -0600

    Change card styling
    Changed the card styling to better meet the light mode
    styling.

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -12,20 +12,26 @@ const Card: React.FC<CardProps> = ({ title, image, description, link }) => {
   return (
     <div
       className="flex flex-col justify-center min-h-100 w-75 items-center
-    text-center border-2 rounded-lg overflow-hidden "
+    text-center shadow-lg rounded-lg overflow-hidden bg-white"
     >
-      <p className="p-5 bg-amber-50 w-[100%] mb-5 text-black font-extrabold">
+      <p className="p-5 bg-gray-100 w-[100%] mb-5 text-black font-extrabold">
         {title}
       </p>
       <Image
         src={image}
-        width={200}
+        width={400}
         height={200}
         alt="Image of rivers karate website"
         className="grow mb-5"
       />
-      <p className="grow">{description}</p>
-      <a href={link} target="_blank">
+      <p className="grow p-5">{description}</p>
+      <a
+        href={link}
+        target="_blank"
+        className="m-5 hover:bg-blue-400 p-3 
+        rounded-md hover:text-white font-bold bg-white 
+        text-blue-400 border-blue-400  border"
+      >
         Visit Site
       </a>
     </div>
