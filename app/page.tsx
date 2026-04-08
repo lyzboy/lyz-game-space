11eaebfaafa9e132b2af39d4f180b63697143859
Josh Sanford <13478602+lyzboy@users.noreply.github.com>
2026-04-07 18:21:10 -0600

    Refactor hero section
    On  the main page, I added my site description and a picture
    of Josh.

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,26 @@ import Sites from "./components/Sites";
 export default function Home() {
   return (
     <>
-      <main className="lg:pl-50 lg:pr-50 md:pl-25 md:pr-25">
-        <div>
-          <h1>Lyz Studios</h1>
-          <p>Welcome to lyz studios</p>
+      <main className="lg:pl-50 lg:pr-50 md:pl-25 md:pr-25 pr-15 pl-15">
+        <div className="flex items-center justify-center content-center flex-col sm:flex-row mt-10">
+          <div className="rounded-full w-48 h-48 flex-shrink-0 mr-10 mb-10 overflow-hidden">
+            <img
+              src="/images/Josh_BG_Green.png"
+              alt="an image of Josh"
+              className="object-cover w-full h-full"
+            />
+          </div>
+          <p>
+            Welcome to lyz studios. My name is Josh and this site has been
+            created to as a hub for all of my projects. Feel free to look around
+            and explorer. Not only are some projects that I have created here,
+            but there are also some self host utilities available to use. If you
+            are looking to learn about some technology and see how I have
+            created and hosted some of these utilities, check out some of the
+            blog posts and the learn free learning platform.
+          </p>
         </div>
+
         <Sites />
       </main>
     </>
