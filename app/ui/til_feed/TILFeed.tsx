931a21d9b36ba218e89e049a9b9bf866fe12d679
Josh Sanford <13478602+lyzboy@users.noreply.github.com>
2026-04-08 18:17:47 -0600

    Add TIL feed basic
    Added the TIL component to the main feed. Added the
    basics of the TIL component framework

---
+++ app/ui/til_feed/TILFeed.tsx
@@ -0,0 +1,16 @@
+const TILFeed = () => {
+  return (
+    <div className="mt-10 mb-10 border-blue-400 border-2 rounded-2xl p-5">
+      <div>
+        <h2 className="font-bold text-2xl">
+          Current Focus: Dev Diary Creation
+        </h2>
+        {/* this will be for badges */}
+        <div></div>
+      </div>
+      <a href="#">Repo: lyz-game-space</a>
+    </div>
+  );
+};
+
+export default TILFeed;
