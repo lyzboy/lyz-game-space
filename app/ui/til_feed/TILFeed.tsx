8a581e21dc855257fd393e08e63e7ee1dc7d51cc
Josh Sanford <13478602+lyzboy@users.noreply.github.com>
2026-04-08 20:31:03 -0600

    Add components to TIL feed
    Added the basic components to the TIL feed. There are
    no state or fetch data currently.

--- app/ui/til_feed/TILFeed.tsx
+++ app/ui/til_feed/TILFeed.tsx
@@ -1,14 +1,26 @@
+import Entry from "../components/Entry";
+import AhaEntry from "../components/AhaEntry";
+
 const TILFeed = () => {
   return (
     <div className="mt-10 mb-10 border-blue-400 border-2 rounded-2xl p-5">
+      <h2 className="text-2xl font-bold">Today I Learned Feed</h2>
       <div>
-        <h2 className="font-bold text-2xl">
-          Current Focus: Dev Diary Creation
-        </h2>
+        <p className="font-bold text-xl">Current Focus: Dev Diary Creation</p>
         {/* this will be for badges */}
         <div></div>
       </div>
-      <a href="#">Repo: lyz-game-space</a>
+      <a href="#" className="text-blue-400">
+        Repo: lyz-game-space
+      </a>
+      <p className="font-bold mt-3 text-lg">Latest Entry:</p>
+      <Entry />
+      <div>
+        <p className="font-bold mt-3 text-lg">Recent Aha! Moments:</p>
+        <AhaEntry />
+        <AhaEntry />
+        <AhaEntry />
+      </div>
     </div>
   );
 };
