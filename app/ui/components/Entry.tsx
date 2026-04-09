43e03e4416256fae16dc607d19ced8cf3f6b7be1
Josh Sanford <13478602+lyzboy@users.noreply.github.com>
2026-04-08 20:30:11 -0600

    Add basic data to Entry component

---
+++ app/ui/components/Entry.tsx
@@ -0,0 +1,15 @@
+// A TIL Feed entry
+
+const Entry = () => {
+  return (
+    <div className="border-2 p-2 rounded-2xl">
+      <p className="font-bold">Entry Date: 04/08/26</p>
+      <p>
+        Working on creating uml diagrams for the dev diary process. Utilizing
+        the self...
+      </p>
+    </div>
+  );
+};
+
+export default Entry;
