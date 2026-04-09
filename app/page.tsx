c7fc442e509b4b7f2fc651277eca3b9dce09f0f2
Josh Sanford <13478602+lyzboy@users.noreply.github.com>
2026-04-08 18:17:47 -0600

    Add TIL feed basic
    Added the TIL component to the main feed. Added the
    basics of the TIL component framework

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Sites from "./ui/components/Sites";
 import AuthorCard from "./ui/components/AuthorCard";
+import TILFeed from "./ui/til_feed/TILFeed";
 
 export default function Home() {
   return (
     <>
       <main className="lg:pl-50 lg:pr-50 md:pl-25 md:pr-25 pr-15 pl-15">
         <AuthorCard />
-
+        <TILFeed />
         <Sites />
       </main>
     </>
