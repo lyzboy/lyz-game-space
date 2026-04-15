d1c8e7898d1b683c0f9621798433364ef9acaa66
Josh Sanford <13478602+lyzboy@users.noreply.github.com>
2026-04-15 09:35:13 -0600

    Add the entry form for the admin page
    The entry form will be used to create new entries by
    the admin.

---
+++ app/ui/components/EntryForm.tsx
@@ -0,0 +1,12 @@
+/**
+ * A component used by the admin of the site to create new entries.
+ * @returns An entry form component
+ */
+
+export default function EntryForm() {
+  return (
+    <div>
+      <p>Entry Form</p>
+    </div>
+  );
+}
