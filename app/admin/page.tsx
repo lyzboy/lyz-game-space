3cdb7aaa67bb605c806e17fb1bddbb6fad89e113
Josh Sanford <13478602+lyzboy@users.noreply.github.com>
2026-04-15 09:34:51 -0600

    Create the admin page basic structure

---
+++ app/admin/page.tsx
@@ -0,0 +1,13 @@
+import EntryForm from "../ui/components/EntryForm";
+
+/**
+ * The administration page for the website. This is a protected route.
+ * @returns The admin page
+ */
+export default function Admin() {
+  return (
+    <div>
+      <EntryForm />
+    </div>
+  );
+}
