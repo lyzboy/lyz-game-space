b98edf5d6de2b2d9ef3a3ce49c4ba0ab06644cd5
Josh Sanford <13478602+lyzboy@users.noreply.github.com>
2026-04-15 20:30:14 -0600

    Add button to main to go to focus view page

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,13 @@ export default function Home() {
       <main className="lg:pl-50 lg:pr-50 md:pl-25 md:pr-25 pr-15 pl-15">
         <AuthorCard />
         <TILFeed />
+        <a
+          className="bg-blue-400 p-3 rounded-2xl text-white font-bold
+        cursor-pointer"
+          href="/focuses"
+        >
+          View All Focuses
+        </a>
         <Sites />
       </main>
     </>
