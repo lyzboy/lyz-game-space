1fe7d3871654ae86d528dde29c00b8e328f19b57
Josh Sanford <13478602+lyzboy@users.noreply.github.com>
2026-05-14 12:38:44 -0600

    WIP: styline the author card
    Added icons for github and linked in

--- app/ui/components/AuthorCard.tsx
+++ app/ui/components/AuthorCard.tsx
@@ -1,34 +1,34 @@
 import Image from "next/image";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
+
+import { SiGithub } from "@icons-pack/react-simple-icons";
 
 const AuthorCard = () => {
   return (
-    <div className="grid grid-cols-4 grid-rows-4 lg:grid-rows-3 w-full">
+    <div className="flex flex-col w-full">
       <div
-        className="rounded-full w-48 h-48 flex-shrink-0 mr-10 
-      overflow-hidden"
+        className="rounded-full w-48 h-48 mr-10 
+      overflow-hidden mb-6"
       >
         <Image
           src="/images/Josh_BG_Green.png"
           alt="an image of Josh"
-          className="object-cover w-full h-full col-span-1 row-span-1"
+          className="object-cover w-full h-full col-span-1 row-span-1 lg:col-span-3"
           width={300}
           height={300}
         />
       </div>
-      <p>Hi, I'm Josh</p>
-      <div>
+      <p className="font-bold text-3xl mb-4">Hi, I'm Josh</p>
+      <div className="mb-4">
         <p>
           Welcome to <strong>lyz studios</strong>. I’m Josh, a Full-Stack
           Developer specializing in the PERN stack and Next.js. This dashboard
           serves as a live showcase of my deployed projects and technical
-          accomplishments.
-        </p>
-        <br />
-        <p>
-          To demonstrate end-to-end infrastructure management, every application
-          found here is self-hosted on my local home server—optimized for
-          security and performance via Cloudflare Tunnels and Docker
-          containerization.
+          accomplishments. To demonstrate end-to-end infrastructure management,
+          every application found here is self-hosted on my local home
+          server—optimized for security and performance via Cloudflare Tunnels
+          and Docker containerization.
         </p>
         <br />
         <p>
@@ -38,9 +38,35 @@ const AuthorCard = () => {
           access my hosted applications.
         </p>
       </div>
-      <div>
-        <p>GitHub</p>
-        <p>LinkedIn</p>
+      <div className="flex gap-2">
+        <a
+          href="https://www.github.com/lyzboy"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={cn(
+            buttonVariants({
+              size: "lg",
+            }),
+
+            "bg-black rounded-full",
+          )}
+        >
+          <SiGithub color="#FFFFFF" size={24} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/joshuaraysanford"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={cn(
+            buttonVariants({
+              size: "lg",
+            }),
+
+            "bg-black rounded-full",
+          )}
+        >
+          <Image src="/InBug-White.png" alt="" width="25" height="25" />
+        </a>
       </div>
     </div>
   );
