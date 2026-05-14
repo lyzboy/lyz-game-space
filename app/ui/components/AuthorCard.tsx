340137d43e8d2837b935a2c207e7f163368661e1
Josh Sanford <13478602+lyzboy@users.noreply.github.com>
2026-05-14 11:02:32 -0600

    WIP: begin refactor of author card with shad

--- app/ui/components/AuthorCard.tsx
+++ app/ui/components/AuthorCard.tsx
@@ -1,21 +1,22 @@
+import Image from "next/image";
+
 const AuthorCard = () => {
   return (
-    <div
-      className="flex items-center justify-center content-center flex-col 
-    sm:flex-row mt-10 border-2 border-blue-400 p-10 rounded-4xl shadow-xl"
-    >
+    <div className="grid grid-cols-4 grid-rows-4 lg:grid-rows-3 w-full">
       <div
         className="rounded-full w-48 h-48 flex-shrink-0 mr-10 
       overflow-hidden"
       >
-        <img
+        <Image
           src="/images/Josh_BG_Green.png"
           alt="an image of Josh"
-          className="object-cover w-full h-full"
+          className="object-cover w-full h-full col-span-1 row-span-1"
+          width={300}
+          height={300}
         />
       </div>
+      <p>Hi, I'm Josh</p>
       <div>
-        <p className="text-blue-400 font-bold text-2xl mb-3">Josh Sanford</p>
         <p>
           Welcome to <strong>lyz studios</strong>. I’m Josh, a Full-Stack
           Developer specializing in the PERN stack and Next.js. This dashboard
@@ -36,6 +37,10 @@ const AuthorCard = () => {
           <a className="text-blue-400 cursor-pointer">Sites Section</a> to
           access my hosted applications.
         </p>
+      </div>
+      <div>
+        <p>GitHub</p>
+        <p>LinkedIn</p>
       </div>
     </div>
   );
