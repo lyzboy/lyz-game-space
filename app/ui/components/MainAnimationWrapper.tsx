79ccbfd72b03db731e2ac1feac5b755dd78cf2f8
Josh Sanford <13478602+lyzboy@users.noreply.github.com>
2026-05-15 17:14:05 -0600

    WIP: added use effect for animation on main page

--- app/ui/components/MainAnimationWrapper.tsx
+++ app/ui/components/MainAnimationWrapper.tsx
@@ -7,10 +7,40 @@ interface Props {
   children: ReactNode;
 }
 const REVEAL_TIMER = 2000;
+const TRANSITION_TIMER = 500;
+const BLUR_TIMER = 1500;
 export default function MainAnimationWrapper({ children }: Props) {
   const [phase, setPhase] = useState<
     "intro" | "transition" | "curtain" | "complete"
   >("intro");
+
+  useEffect(() => {
+    switch (phase) {
+      case "intro":
+        console.log(`in intro phase...`);
+        const firstTimer = setTimeout(
+          () => setPhase("transition"),
+          REVEAL_TIMER,
+        );
+        return () => clearTimeout(firstTimer);
+      case "transition":
+        console.log(`in transition phase...`);
+        const secondTimer = setTimeout(
+          () => setPhase("curtain"),
+          TRANSITION_TIMER,
+        );
+        return () => clearTimeout(secondTimer);
+      case "curtain":
+        console.log(`in curtain phase...`);
+        const thirdTimer = setTimeout(() => setPhase("complete"), BLUR_TIMER);
+        return () => clearTimeout(thirdTimer);
+      case "complete":
+        console.log("animation completed!");
+        return;
+      default:
+        return;
+    }
+  }, [phase]);
   return (
     <div>
       <AuthorCard />
