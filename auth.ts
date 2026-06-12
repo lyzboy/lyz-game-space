12b2502dfb6a77d0c467bea076580fec3b116444
Josh Sanford <13478602+lyzboy@users.noreply.github.com>
2026-06-12 11:18:45 -0600

    Add domain path to auth.ts

--- auth.ts
+++ auth.ts
@@ -33,6 +33,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         sameSite: "lax",
         path: "/",
         secure: true,
+        domain: ".lyzgame.space",
       },
     },
   },
